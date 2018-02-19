7d76e9ce52a95c53d4625b1eff982272d6195537
josh-lovell <36432593+josh-lovell@users.noreply.github.com>
2018-02-18 22:41:54 -0500

    Add files via upload

---
+++ symbolizer.js
@@ -0,0 +1,6 @@
+let myName = 'Josh';
+let myCity = 'Ellicott City';
+const myAge = '19';
+console.log(`My name is ${myName}. I am from
+ ${myCity} and I am ${myAge} years old.`);
+// a statement about myself
